674ee184af00ab179cceeb59d481e91bd8f6117c
Tito <vicente.ict3@gmail.com>
2019-08-05 19:54:00 +0200

    2019-08-05T19:54Z

--- gitBatch.bat
+++ gitBatch.bat
@@ -1,6 +1,6 @@
 @echo off
-for /f "tokens=1-3 delims=/- " %%a in ('date /t') do set FECHA=%%c%%b%%a
-for /f "tokens=1-2 delims=: " %%a in ('time /t') do set HORA=%%a%%b
+for /f "tokens=1-3 delims=/- " %%a in ('date /t') do set FECHA=%%c-%%b-%%a
+for /f "tokens=1-2 delims=: " %%a in ('time /t') do set HORA=%%a:%%b
 echo %FECHA%T%HORA%Z
 git add .
 git status
